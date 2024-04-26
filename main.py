dfb189f04f82c1c2fa698a3da5145330bf1633d0
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 21:58:13 +0700

    One-line docstrings

--- main.py
+++ main.py
@@ -16,3 +16,10 @@ if __name__ == "__main__":
   """
   Unlike a regular comment, a documentation string can be accessed at run-time using obj.__doc__ attribute when obj is the name of the function.
   """
+
+  # One-line docstrings
+
+  def quicksort():
+    """sort the list using quicksort algorithm"""
+
+  print(quicksort.__doc__)
