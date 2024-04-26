9f33c494956e377aaa5a72065b34080021ac4c89
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 21:55:49 +0700

    Python docstrings

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ if __name__ == "__main__":
 
   salary = 100
   salary*= 1.02 # increase salary by 2%
+
+  # Python docstrings
+
+  """
+  Unlike a regular comment, a documentation string can be accessed at run-time using obj.__doc__ attribute when obj is the name of the function.
+  """
